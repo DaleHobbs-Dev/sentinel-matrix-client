c9a93028d5c8ea5c10327f8404e3cb03ffac7360
Dale Hobbs <dale.hobbs.dev@gmail.com>
2026-06-22 18:28:10 -0500

    Adjusted Sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,19 +1,26 @@
 import { SidebarLink } from "@/components/ui"
+import logo from "@/assets/logo.png"
 
 export const Sidebar = () => {
   return (
-    <aside className="w-64 bg-white border-r border-gray-200">
-        <div className="p-4 border-b border-slate-200">
-            <img src="../../assets/logo.png" alt="Sentinel Matrix"/>
-            <h1 className="text-xl font-bold mt-2">Sentinel Matrix</h1>
-        </div>
+    <aside className="w-64 shrink-0 border-r border-slate-200 bg-white">
+      <div className="border-b border-slate-200 p-4">
+        <img
+          src={logo}
+          alt="Sentinel Matrix"
+          className="h-12 w-auto"
+        />
+        <h1 className="mt-2 text-xl font-bold text-primary-dark">
+          Sentinel Matrix
+        </h1>
+      </div>
 
-      <nav className="sidebar-nav p-3 space-y-2">
+      <nav className="flex flex-col gap-2 p-3">
         <SidebarLink to={"/dashboard"}>Dashboard</SidebarLink>
         <SidebarLink to={"/"}>Courses</SidebarLink>
         <SidebarLink to={"/"}>Students</SidebarLink>
         <SidebarLink to={"/"}>Analytics</SidebarLink>
       </nav>
     </aside>
-    );
+  );
 }
